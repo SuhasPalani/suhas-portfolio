2e21919bf8904e9a743312a1a34bd45d3454de8a
Suhas Palani <sugipals007@gmail.com>
2024-02-28 14:44:09 -0600

    OVERVIEW DONE-end of style scripting

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// typinggggggg  
+
+var typed = new Typed(".typing", {
+    strings: ["", "Software Developer", "Web Designer", "Web Developer", "Musician", "DevOps Engineer"],
+    typeSpeed: 50,
+    backSpeed: 10,
+    loop: true
+});
