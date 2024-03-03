03976472c8bbf9a3406c0cfbd2579f664fa9378a
Suhas Palani <sugipals007@gmail.com>
2024-03-03 09:31:18 -0600

    final update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // typinggggggg  
 
 var typed = new Typed(".typing", {
-    strings: ["", "Software Developer", "Web Designer", "Web Developer", "Musician", "DevOps Engineer"],
+    strings: ["", "Software Developer", "Web Developer", "Musician", "DevOps Engineer"],
     typeSpeed: 50,
     backSpeed: 10,
     loop: true
